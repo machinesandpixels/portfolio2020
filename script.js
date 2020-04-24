a8b9caa5ede2776f01343b82fe509c3ad8f878ea
JQ <2016quezadaj@gmail.com>
2020-04-24 13:42:56 -0700

    Added script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+let i = 0;
+let txt = 'Hi, I am a self taught developer and bootcamp grad.';
+let speed = 50;
+
+// function typeWriter() {
+//     if (i < txt.length) {
+//       document.getElementById("typing").innerHTML += txt.charAt(i);
+//       i++;
+//       setTimeout(typeWriter, speed);
+//     }
+//   }
