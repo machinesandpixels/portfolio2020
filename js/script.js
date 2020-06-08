3acaebc35f617630556d9c3f8052b26539f4aa3e
jesusquezada <2016quezadaj@gmail.com>
2020-06-07 23:00:35 -0700

    Added some JS code to fix background video issue on Edge Browser

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 // Check if browser is Edge
 if (/Edge/.test(navigator.userAgent)) {
   alert('Hello Microsoft User!');
+  let video = document.getElementById("video");
+  // video.style.minHeight = '100%';
+  // video.style.minWidth = "100%";
+  // video.style.height = auto;
+  // video.style.weight = auto;
+  // video.style.left = '50%';
+  // video.style.top = '50%';
+  // video.style.transform = 'translate(-50%, -50%)';
+     /* min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%); */
 }
 
 const letters = document.querySelector('.typing');
